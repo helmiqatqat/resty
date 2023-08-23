3aa793a09d4b55eee297b2829317bc516a9093ec
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-08-23 17:05:13 +0000

    reducer hook used and history added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 
 import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Form from './components/Form/Form';
 import Results from './components/Results/Results';
-
+import History from './components/History/History';
+const requestsReducer = (requests, actions) => {
+  const {url, method, result, type} = actions;
+  console.log(actions)
+  switch(type) {
+    case 'api-call':
+      return [
+        ...requests,
+        {
+          url: url,
+          method: method,
+        }
+      ];
+  }
+}
 export default function App () {
   const [data, setData] = useState(null)
   const [requestParams, setRequestParams] = useState({
@@ -14,13 +28,14 @@ export default function App () {
     body: ""
   })
   const [selected, setSetelected] = useState("")
+  const [requests, dispatch] = useReducer(requestsReducer, [])
 
   useEffect(() => {
     const {url, method} = requestParams
     if(url && method) {
       callApi(requestParams)
     }
-  }, [data, requestParams])
+  }, [requestParams])
   
   const callApi = (requestParams) => {
     const {url, method, body} = requestParams
@@ -29,14 +44,31 @@ export default function App () {
         method: method
       })
       .then(result => result.json())
-      .then(result => setData(result))
+      .then(result => {
+        setData(result)
+        dispatch({
+          type: 'api-call',
+          url: url,
+          method: method,
+          result: result
+        })
+      })
     } else {
       fetch(url, {
         method: method,
         body: body
       })
       .then(result => result.json())
-      .then(result => setData(result))
+      .then(result => {
+        setData(result)
+        dispatch({
+          type: 'api-call',
+          url: url,
+          method: method,
+          result: result
+        })
+      })
+      
     }
   }
   return (
@@ -50,6 +82,7 @@ export default function App () {
           setSetelected={setSetelected}
           />
         <Results data={data} />
+        <History requests={requests}/>
         <Footer />
       </React.Fragment>
   )
--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -4,6 +4,7 @@ import 'react-json-pretty/themes/monikai.css';
 export default function Results({data}) {
   return (
     <div data-testid ='results'>
+      <h2>Result</h2>
     {data && <JSONPretty id="json-pretty" data={data}/>}
     </div>
   )
